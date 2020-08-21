0dc8a14236a158fbd4a446205f8e4fecd67ebdaa
Yavuz Özer <49324804+yavuzerenozer@users.noreply.github.com>
2020-08-21 13:55:53 +0300

    init

---
+++ precache-manifest.4d06a95181daf5776141cbaf46159dff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af8f3e984464f3ff241027b2e201160",
+    "url": "/todo-react-redux/index.html"
+  },
+  {
+    "revision": "d5fcae1782d697988cc2",
+    "url": "/todo-react-redux/static/css/2.537efa30.chunk.css"
+  },
+  {
+    "revision": "3ce5c83734ef7aad5bc8",
+    "url": "/todo-react-redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5fcae1782d697988cc2",
+    "url": "/todo-react-redux/static/js/2.cee4d42e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-react-redux/static/js/2.cee4d42e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce5c83734ef7aad5bc8",
+    "url": "/todo-react-redux/static/js/main.8522388a.chunk.js"
+  },
+  {
+    "revision": "f2fb28eda2ea8dca37a2",
+    "url": "/todo-react-redux/static/js/runtime-main.dc4e1c01.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todo-react-redux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todo-react-redux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todo-react-redux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo-react-redux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todo-react-redux/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
